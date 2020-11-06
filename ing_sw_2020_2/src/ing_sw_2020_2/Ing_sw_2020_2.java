3d47d8533d0cff8f257ca57d67c6523c1eda6047
Usuario <Usuario@DESKTOP-Q2QFFP4>
2020-11-05 22:20:56 -0500

    Version 1.0 integracion de netbeans con github

---
+++ ing_sw_2020_2/src/ing_sw_2020_2/Ing_sw_2020_2.java
@@ -0,0 +1,18 @@
+
+package ing_sw_2020_2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ing_sw_2020_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenido");
+        System.out.println("Integrando Netbeans con github");
+    }
+    
+}
