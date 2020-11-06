3c2f7be59877a8f4a6fecabf00ed0e2563121aa5
Usuario <Usuario@DESKTOP-Q2QFFP4>
2020-11-06 00:56:41 -0500

    Version 0 integrando netbeans con github

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -15,11 +15,8 @@ public class Cv_ejercicio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // TODO code application logic here
         System.out.println("Versión 0");
-        System.out.println("Versión 1");
-        System.out.println("Nilsa Satova");
-        
-       
     }
     
 }
